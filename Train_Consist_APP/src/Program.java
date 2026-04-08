2a75429a0fdc5a57a62e0522cc20160e4ec52c59
varshilworld <varshilgupta@gmail.com>
2026-04-08 09:01:13 +0530

    UC1 completed

---
+++ Train_Consist_APP/src/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("==================");
+        System.out.println("==Train Consist Management APP==");
+        System.out.println("==================");
+        System.out.println("Train Initialised Successfully");
+        System.out.println("Initial bogie count : 0");
+        System.out.println("Current Train Consist : []");
+        System.out.println("Systems ready for operations");
+    }
+}
