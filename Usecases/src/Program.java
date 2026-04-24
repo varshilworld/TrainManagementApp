bc18cb4d4771c66fed12f4214d7b755d03e41e6d
varshilworld <varshilgupta@gmail.com>
2026-04-24 09:53:50 +0530

    usecase

--- Usecases/src/Program.java
+++ Usecases/src/Program.java
@@ -1,19 +1,14 @@
 import java.util.*;
 public class Program {
     public static void main(String[] args) {
-        System.out.println("UC4");
-        List<String> trainConsist = new LinkedList<>();
-        trainConsist.addFirst("Engine");
-        trainConsist.addLast("Guard");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        System.out.println("initial train consist : \n"+trainConsist);
-        trainConsist.add(2,"Pantry");
-        System.out.println("After adding Pantry at index 2 : \n"+trainConsist);
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-        System.out.println("After removing First and last bogie : \n"+trainConsist);
-        System.out.println("UC4 implemented");
+        System.out.println("UC5");
+        Set <String> formation = new LinkedHashSet<>();
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("AC");
+        formation.add("Guard");
+        formation.add("Engine");
+        System.out.println(formation);
+        System.out.println("UC5 implemented");
     }
 }
